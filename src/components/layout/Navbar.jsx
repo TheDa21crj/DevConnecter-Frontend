46d5eee36d2bf8e71db868583bebcab7303e5dd9
Rishav Singh <rishavrajcrj@gmail.com>
2024-04-08 21:54:19 +0530

    Update Navbar.jsx

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -25,13 +25,13 @@ export default function Navbar() {
       </li>
       <li>
         <Link to="/dashboard">
-          <i className="fas fa-user" />{" "}
+          <i className="fas fa-user" />
           <span className="hide-sm">Dashboard</span>
         </Link>
       </li>
       <li>
         <a onClick={logout} href="#!">
-          <i className="fas fa-sign-out-alt" />{" "}
+          <i className="fas fa-sign-out-alt" />
           <span className="hide-sm">Logout</span>
         </a>
       </li>
