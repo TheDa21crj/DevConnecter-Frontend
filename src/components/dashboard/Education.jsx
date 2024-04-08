6662b4780f4eccd55b62018e5f79c007b0415474
Rishav Singh <rishavrajcrj@gmail.com>
2024-04-08 23:49:13 +0530

    Update Education.jsx

--- src/components/dashboard/Education.jsx
+++ src/components/dashboard/Education.jsx
@@ -1,20 +1,20 @@
-import React, { Fragment } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { deleteEducation } from '../../actions/profile';
-import formatDate from '../../utils/formatDate';
+import React, { Fragment } from "react";
+// import PropTypes from 'prop-types';
+// import { connect } from 'react-redux';
+// import { deleteEducation } from '../../actions/profile';
+// import formatDate from '../../utils/formatDate';
 
-const Education = ({ education, deleteEducation }) => {
+const Education = ({ education }) => {
   const educations = education.map((edu) => (
     <tr key={edu._id}>
       <td>{edu.school}</td>
       <td className="hide-sm">{edu.degree}</td>
       <td>
-        {formatDate(edu.from)} - {edu.to ? formatDate(edu.to) : 'Now'}
+        {formatDate(edu.from)} - {edu.to ? formatDate(edu.to) : "Now"}
       </td>
       <td>
         <button
-          onClick={() => deleteEducation(edu._id)}
+          // onClick={() => deleteEducation(edu._id)}
           className="btn btn-danger"
         >
           Delete
@@ -41,9 +41,4 @@ const Education = ({ education, deleteEducation }) => {
   );
 };
 
-Education.propTypes = {
-  education: PropTypes.array.isRequired,
-  deleteEducation: PropTypes.func.isRequired
-};
-
-export default connect(null, { deleteEducation })(Education);
+export default Education;
