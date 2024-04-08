a55e9c5a79c3e5d3988a39b282a1fb300c27b0df
Rishav Singh <rishavrajcrj@gmail.com>
2024-04-08 21:40:12 +0530

    Update Dashboard.jsx

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -10,6 +10,8 @@ import DashboardActions from "./DashboardActions";
 // state
 import AuthContext from "./../../store/auth-context";
 
+import axios from "axios";
+
 const Dashboard = () =>
   // {
   // getCurrentProfile,
@@ -25,7 +27,9 @@ const Dashboard = () =>
     const getCurrentProfile = async () => {
       console.log("Fetching Profiles...");
       try {
-        const res = await api.get("/api/profile/me");
+        const res = await axios.get("/api/profile/me", {
+          headers: { "x-auth-token": `${authCtx.token}` },
+        });
 
         console.log(res);
       } catch (error) {
@@ -38,36 +42,37 @@ const Dashboard = () =>
     }, []);
 
     return (
-      <section className="container">
-        <h1 className="large text-primary">Dashboard</h1>
-        <p className="lead">
-          <i className="fas fa-user" /> Welcome
-          {/* {user && user.name} */}
-        </p>
-        {profile !== null ? (
-          <>
-            {/* <DashboardActions /> */}
-            {/* <Experience experience={profile.experience} /> */}
-            {/* <Education education={profile.education} /> */}
+      // <section className="container">
+      //   <h1 className="large text-primary">Dashboard</h1>
+      //   <p className="lead">
+      //     <i className="fas fa-user" /> Welcome
+      //     {/* {user && user.name} */}
+      //   </p>
+      //   {profile !== null ? (
+      //     <>
+      //       {/* <DashboardActions /> */}
+      //       {/* <Experience experience={profile.experience} /> */}
+      //       {/* <Education education={profile.education} /> */}
 
-            <div className="my-2">
-              <button
-                className="btn btn-danger"
-                // onClick={() => deleteAccount()}
-              >
-                <i className="fas fa-user-minus" /> Delete My Account
-              </button>
-            </div>
-          </>
-        ) : (
-          <>
-            <p>You have not yet setup a profile, please add some info</p>
-            <Link to="/create-profile" className="btn btn-primary my-1">
-              Create Profile
-            </Link>
-          </>
-        )}
-      </section>
+      //       <div className="my-2">
+      //         <button
+      //           className="btn btn-danger"
+      //           // onClick={() => deleteAccount()}
+      //         >
+      //           <i className="fas fa-user-minus" /> Delete My Account
+      //         </button>
+      //       </div>
+      //     </>
+      //   ) : (
+      //     <>
+      //       <p>You have not yet setup a profile, please add some info</p>
+      //       <Link to="/create-profile" className="btn btn-primary my-1">
+      //         Create Profile
+      //       </Link>
+      //     </>
+      //   )}
+      // </section>
+      <div>Hello</div>
     );
   };
 
