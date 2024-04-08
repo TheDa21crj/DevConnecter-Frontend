0e449df57aa02a475e2c44db0f8eac886b81ec47
Rishav Singh <rishavrajcrj@gmail.com>
2024-04-08 21:57:29 +0530

    Update Dashboard.jsx

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -47,37 +47,37 @@ const Dashboard = () =>
     }, []);
 
     return (
-      // <section className="container">
-      //   <h1 className="large text-primary">Dashboard</h1>
-      //   <p className="lead">
-      //     <i className="fas fa-user" /> Welcome
-      //     {/* {user && user.name} */}
-      //   </p>
-      //   {profile !== null ? (
-      //     <>
-      //       {/* <DashboardActions /> */}
-      //       {/* <Experience experience={profile.experience} /> */}
-      //       {/* <Education education={profile.education} /> */}
+      <section className="container">
+        <h1 className="large text-primary">Dashboard</h1>
+        <p className="lead">
+          <i className="fas fa-user" /> Welcome
+          {authCtx.user.name}
+        </p>
 
-      //       <div className="my-2">
-      //         <button
-      //           className="btn btn-danger"
-      //           // onClick={() => deleteAccount()}
-      //         >
-      //           <i className="fas fa-user-minus" /> Delete My Account
-      //         </button>
-      //       </div>
-      //     </>
-      //   ) : (
-      //     <>
-      //       <p>You have not yet setup a profile, please add some info</p>
-      //       <Link to="/create-profile" className="btn btn-primary my-1">
-      //         Create Profile
-      //       </Link>
-      //     </>
-      //   )}
-      // </section>
-      <div>Hello</div>
+        {dataShow.length === 0 ? (
+          <>
+            <p>You have not yet setup a profile, please add some info</p>
+            <Link to="/create-profile" className="btn btn-primary my-1">
+              Create Profile
+            </Link>
+          </>
+        ) : (
+          <>
+            {/* <DashboardActions /> */}
+            {/* <Experience experience={profile.experience} /> */}
+            {/* <Education education={profile.education} /> */}
+
+            <div className="my-2">
+              <button
+                className="btn btn-danger"
+                // onClick={() => deleteAccount()}
+              >
+                <i className="fas fa-user-minus" /> Delete My Account
+              </button>
+            </div>
+          </>
+        )}
+      </section>
     );
   };
 
