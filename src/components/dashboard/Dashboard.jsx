1c09c7e15ffabe9ab1f1667acf21633d93498a35
Rishav Singh <rishavrajcrj@gmail.com>
2024-04-08 21:32:28 +0530

    Update Dashboard.jsx

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useContext, useState } from "react";
 import { Link } from "react-router-dom";
 // import PropTypes from "prop-types";
 // import { connect } from "react-redux";
@@ -7,50 +7,68 @@ import DashboardActions from "./DashboardActions";
 // import Education from "./Education";
 // import { getCurrentProfile, deleteAccount } from "../../actions/profile";
 
-const Dashboard = (
+// state
+import AuthContext from "./../../store/auth-context";
+
+const Dashboard = () =>
+  // {
+  // getCurrentProfile,
+  // deleteAccount,
+  // auth: { user },
+  // profile: { profile },
+  // }
   {
-    // getCurrentProfile,
-    // deleteAccount,
-    // auth: { user },
-    // profile: { profile },
-  }
-) => {
-  // useEffect(() => {
-  //   getCurrentProfile();
-  // }, [getCurrentProfile]);
+    const [dataShow, setData] = useState([]);
 
-  return (
-    // <section className="container">
-    //   <h1 className="large text-primary">Dashboard</h1>
-    //   <p className="lead">
-    //     <i className="fas fa-user" /> Welcome {user && user.name}
-    //   </p>
-    //   {profile !== null ? (
-    //     <>
-    //       {/* <DashboardActions /> */}
-    //       {/* <Experience experience={profile.experience} /> */}
-    //       {/* <Education education={profile.education} /> */}
+    const authCtx = useContext(AuthContext);
 
-    //       <div className="my-2">
-    //         <button
-    //           className="btn btn-danger"
-    //           // onClick={() => deleteAccount()}
-    //         >
-    //           <i className="fas fa-user-minus" /> Delete My Account
-    //         </button>
-    //       </div>
-    //     </>
-    //   ) : (
-    //     <>
-    //       <p>You have not yet setup a profile, please add some info</p>
-    //       <Link to="/create-profile" className="btn btn-primary my-1">
-    //         Create Profile
-    //       </Link>
-    //     </>
-    //   )}
-    // </section>
-    <div>Hello</div>
-  );
-};
+    const getCurrentProfile = async () => {
+      console.log("Fetching Profiles...");
+      try {
+        const res = await api.get("/api/profile/me");
+
+        console.log(res);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    useEffect(() => {
+      getCurrentProfile();
+    }, []);
+
+    return (
+      <section className="container">
+        <h1 className="large text-primary">Dashboard</h1>
+        <p className="lead">
+          <i className="fas fa-user" /> Welcome
+          {/* {user && user.name} */}
+        </p>
+        {profile !== null ? (
+          <>
+            {/* <DashboardActions /> */}
+            {/* <Experience experience={profile.experience} /> */}
+            {/* <Education education={profile.education} /> */}
+
+            <div className="my-2">
+              <button
+                className="btn btn-danger"
+                // onClick={() => deleteAccount()}
+              >
+                <i className="fas fa-user-minus" /> Delete My Account
+              </button>
+            </div>
+          </>
+        ) : (
+          <>
+            <p>You have not yet setup a profile, please add some info</p>
+            <Link to="/create-profile" className="btn btn-primary my-1">
+              Create Profile
+            </Link>
+          </>
+        )}
+      </section>
+    );
+  };
 
 export default Dashboard;
