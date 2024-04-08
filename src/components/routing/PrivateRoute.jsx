b4be8420b010d57484ae3d8ca320e3e1f2288aa8
Rishav Singh <rishavrajcrj@gmail.com>
2024-04-08 21:54:06 +0530

    Update PrivateRoute.jsx

--- src/components/routing/PrivateRoute.jsx
+++ src/components/routing/PrivateRoute.jsx
@@ -7,10 +7,7 @@ import Spinner from "../layout/Spinner";
 // state
 import AuthContext from "./../../store/auth-context";
 
-const PrivateRoute = ({
-  component: Component,
-  // auth: { isAuthenticated, loading },
-}) => {
+const PrivateRoute = ({ component: Component }) => {
   const authCtx = useContext(AuthContext);
 
   // if (loading) return <Spinner />;
