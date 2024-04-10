be720afb98046fcf3b9d286eca2be3ffc0f0ffec
Rishav Singh <rishavrajcrj@gmail.com>
2024-04-10 18:02:22 +0530

    Update ProfileGithub.jsx

--- src/components/profile/ProfileGithub.jsx
+++ src/components/profile/ProfileGithub.jsx
@@ -1,17 +1,18 @@
-import React, { useEffect } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { getGithubRepos } from '../../actions/profile';
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+// import { getGithubRepos } from "../../actions/profile";
 
-const ProfileGithub = ({ username, getGithubRepos, repos }) => {
-  useEffect(() => {
-    getGithubRepos(username);
-  }, [getGithubRepos, username]);
+const ProfileGithub = ({ username }) => {
+  console.log(username);
+
+  // useEffect(() => {
+  //   getGithubRepos(username);
+  // }, [getGithubRepos, username]);
 
   return (
     <div className="profile-github">
       <h2 className="text-primary my-1">Github Repos</h2>
-      {repos.map(repo => (
+      {/* {repos.map((repo) => (
         <div key={repo.id} className="repo bg-white p-1 my-1">
           <div>
             <h4>
@@ -33,7 +34,7 @@ const ProfileGithub = ({ username, getGithubRepos, repos }) => {
             </ul>
           </div>
         </div>
-      ))}
+      ))} */}
     </div>
   );
 };
@@ -41,11 +42,7 @@ const ProfileGithub = ({ username, getGithubRepos, repos }) => {
 ProfileGithub.propTypes = {
   getGithubRepos: PropTypes.func.isRequired,
   repos: PropTypes.array.isRequired,
-  username: PropTypes.string.isRequired
+  username: PropTypes.string.isRequired,
 };
 
-const mapStateToProps = state => ({
-  repos: state.profile.repos
-});
-
-export default connect(mapStateToProps, { getGithubRepos })(ProfileGithub);
+export default ProfileGithub;
